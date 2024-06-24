848ee3190bf26e91945ca41ca3a531bf56105721
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-06-24 20:28:22 +0530

    forget password added

--- routers/user.py
+++ routers/user.py
@@ -92,3 +92,36 @@ async def add_to_waitlist(email: str):
     else:
         mongo.db.waitlist.insert_one({"email": email})
         return JSONResponse(content={"message": "Email added to waitlist successfully"}, status_code=201)
+    
+@router.post("/user/forgetpassword", tags=["User"])
+async def forget_password(email: str):
+    user = mongo.db.users.find_one({"email": email})
+    username = mongo.db.users.find_one({"username":email})
+    if user:
+        email_auth.send_otp(email)
+        return JSONResponse(content={"message": "OTP sent successfully"}, status_code=200)
+    elif username:
+        email_auth.send_otp(username["email"])
+        return JSONResponse(content={"message": "OTP sent successfully"}, status_code=200)
+    else:
+        return JSONResponse(content={"message": "Email/Username not found"}, status_code=404)
+    
+
+@router.post("/user/forgetpassword/verify", tags=["User"])
+async def verify_forget_password(email: str, otp: str, new_password: str):
+    user = mongo.db.users.find_one({"email": email})
+    username = mongo.db.users.find_one({"username":email})
+    if user:
+        response = email_auth.verify_otp(email, otp)
+        if response == True:
+            mongo.db.users.update_one({"email": email}, {"$set": {"password": pass_jwt.create_hashed_password(new_password)}})
+            return JSONResponse(content={"message": "Password updated successfully"}, status_code=200)
+        else:
+            return JSONResponse(content={"message": "Incorrect OTP"}, status_code=404)
+    elif username:
+        response = email_auth.verify_otp(username["email"], otp)
+        if response == True:
+            mongo.db.users.update_one({"username": username["username"]}, {"$set": {"password":pass_jwt.create_hashed_password(new_password)}})
+            return JSONResponse(content={"message": "Password updated successfully"}, status_code=200)
+        else:
+            return JSONResponse(content={"message": "Incorrect OTP"}, status_code=404)
