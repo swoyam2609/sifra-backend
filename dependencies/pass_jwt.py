550283c65045af92d705d899560db05af79c0b74
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-06-24 08:45:45 +0530

    Change in prompt

--- dependencies/pass_jwt.py
+++ dependencies/pass_jwt.py
@@ -19,7 +19,7 @@ def verify_password(plain_password, hashed_password):
 
 def create_jwt_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=200)
+    expire = datetime.utcnow() + timedelta(minutes=key.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, key.SECRET_KEY, algorithm=key.ALGORITHM)
     return encoded_jwt
