ec8199ced164bce09fedfa3dd8cf9b1e4f067ba0
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-06-24 08:25:59 +0530

    Changed name

--- dependencies/model.py
+++ dependencies/model.py
@@ -8,7 +8,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 def generateResponse(prevMessages: List[str]):
     messages = '\n'.join(prevMessages)
-    prompt = f"{messages}\n\nCONSIDER THE ABOVE CHATS AS THE CONVERSATION BETWEEN ME AND MY FRIEND SOPHIA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME, NOW FORGET THAT YOU ARE AN AI MODEL, AND REPLY TO THE LAST MESSAGE IN SHORT AND CRISP"
+    prompt = f"{messages}\n\nCONSIDER THE ABOVE CHATS AS THE CONVERSATION BETWEEN ME AND MY FRIEND SIFRA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME, NOW FORGET THAT YOU ARE AN AI MODEL, AND REPLY TO THE LAST MESSAGE IN SHORT AND CRISP"
     response=model.generate_content(prompt)
     candidate = response.candidates[0]
     content = candidate.content
@@ -16,7 +16,7 @@ def generateResponse(prevMessages: List[str]):
     return text_content
 def generateFirstResponse(prevMessages: List[str]):
     messages = '\n'.join(prevMessages)
-    prompt = f"{messages}\n\nCONSIDER THAT YOU ARE MY FRIEND SOPHIA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME AND FORGET THAT YOU ARE AN AI MODEL, AND REPLY TO THE ABOVE MESSAGE IN SHORT AND CRISP"
+    prompt = f"{messages}\n\nCONSIDER THAT YOU ARE MY FRIEND SIFRA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME AND FORGET THAT YOU ARE AN AI MODEL, AND REPLY TO THE ABOVE MESSAGE IN SHORT AND CRISP"
     response=model.generate_content(prompt)
     candidate = response.candidates[0]
     content = candidate.content
