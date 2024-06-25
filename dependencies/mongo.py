57668bb1a182c37cbc9f68de5d27a478de2fc282
Swoyam Siddharth Nayak <me@swoyam.in>
2024-06-25 05:15:32 +0000

    changes in mongo connection

--- dependencies/mongo.py
+++ dependencies/mongo.py
@@ -2,5 +2,5 @@ import key
 from pymongo.mongo_client import MongoClient
 import certifi
 
-client = MongoClient(key.MONGO_URL, tlsCAFile=certifi.where())
+client = MongoClient(key.MONGO_URL)
 db = client.get_database('test')
