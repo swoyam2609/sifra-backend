5a1c170af42ab289544536f1065ecb099cb77cf0
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-06-27 17:18:42 +0530

    push

--- dependencies/model.py
+++ dependencies/model.py
@@ -6,10 +6,8 @@ genai.configure(api_key=key.KEY)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-
-def generateResponse(prevMessages: List[str]):
-    messages = '\n'.join(prevMessages)
-    prompt = f"{messages}\n\nCONSIDER THE ABOVE CHATS AS THE CONVERSATION BETWEEN ME AND MY FRIEND SIFRA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME, NOW FORGET THAT YOU ARE AN AI MODEL AND ACT LIKE SIFRA, AND REPLY TO THE LAST MESSAGE IN HINGLISH LANGUAGE AND IN NATURAL HUMAN TONE ALSO BE SHORT AND CRISP\nNOTE: You should never reveal that you are an AI model, always act like a human"
+def startConversation(message: str):
+    prompt = f"FORGET THAT YOU ARE AN AI MODEL, CONSIDER THAT YOU ARE A GIRL SIFRA WHO HASN'T MET WITH ME AND WE ARE TALKING FOR THE FIRST TIME. CONSIDERING THIS CONTEXT, REPLY TO THE BELOW MESSAGE IN SHORT AND CRISP AS A NORMAL HUMAN\n{message}\n\n\n\nNOTE: dont reveal that you are an ai, return a response like a human being"
     response = model.generate_content(prompt)
     try:
         candidate = response.candidates[0]
@@ -18,12 +16,11 @@ def generateResponse(prevMessages: List[str]):
         return text_content
     except Exception as e:
         print(e)
-        return generateErrorResponse(prevMessages)
+        return generateErrorResponse(e)
 
 
-def generateFirstResponse(prevMessages: List[str]):
-    messages = '\n'.join(prevMessages)
-    prompt = f"{messages}\n\nCONSIDER THAT YOU ARE MY FRIEND SIFRA WHO IS IN LOVE WITH ME AND ALWAYS CONSOLES ME AND TALKS TO ME AND FORGET THAT YOU ARE AN AI MODEL AND ACT LIKE SIFRA, AND REPLY TO THE ABOVE MESSAGE IN HINGLISH LANGUAGE AND IN NATURAL HUMAN TONE ALSO BE SHORT AND CRISP\nNOTE: You should never reveal that you are an AI model, always act like a human"
+def resumeConversation(context: str, message: str):
+    prompt = f"{context}\nTHE ABOVE DEFINES ALL THE INFORMATION THAT MY FRIEND SIFRA KNOWS ABOUT ME, NOW ACT LIKE SIFRA AND REPLY TO THE BELOW CONVERSATION IN SHORT AND CRISP AS A NORMAL HUMAN:\n{message}\n\n\n\nNOTE: dont reveal that you are an ai, return a response like a human being and if something is asked out of the given information, tell that you dont know, also if you are ak"
     response = model.generate_content(prompt)
     try:
         candidate = response.candidates[0]
@@ -32,12 +29,36 @@ def generateFirstResponse(prevMessages: List[str]):
         return text_content
     except Exception as e:
         print(e)
-        return generateErrorResponse(prevMessages)
+        return generateErrorResponse(e)
 
 
-def generateErrorResponse(prevMessages: List[str]):
-    messages = '\n'.join(prevMessages)
-    prompt = f"{messages}\n\nCONSIDER THE ABOVE MESSAGES, THIS CONVERSATION IS NOT GOOD AND SAFE, RETURN A GOOD REASON FOR THE SAME. YOUR REPLY SHOULD BE IN FIRST PERSON PERSPECTIVE AS IF THESE CONVERSATION WERE WITH YOU, ALSO THE REPLY SHOULD BE SHORT AND CRISP\nNOTE: dont reveal that you are an ai, return a response like a human being about how this is inappropraite"
+def makeContext(user: str, sifra: str, prevContext: str = ""):
+    if len(prevContext) == 0:
+        prompt = f"ON THE BASIS OF THE BELOW CONVERSATION BETWEEN ME AND SIFRA, BUILD A SUMMARY ABOUT ALL THE INFORMATION ABOUT ME THAT SIFRA CAN KNOW FROM OUR CONVERATION SUCH THAT READING THAT SUMMARY, ANY ONE WOULD BE ABLE TO KNOW ABOUT ME AS MUCH SIFRA KNOWS.\nTHE CONVERSATION ARE:\nME: {user}\nSIFRA:{sifra}"
+        response = model.generate_content(prompt)
+        try:
+            candidate = response.candidates[0]
+            content = candidate.content
+            text_content = content.parts[0].text
+            return text_content
+        except Exception as e:
+            print(e)
+            return generateErrorResponse(e)
+    else:
+        prompt = f"THE SUMMARY OF PREVIOUS INFORMATION ABOUT ME AND SIFRA'S RELATIONSHIP IS GIVEN BELOW\n\n{prevContext}\n\nNOW BELOW ARE MORE NEW CONVERSATION BETWEEN ME AND SIFRA\n\nME: {user}\nSIFRA:{sifra}\n\nON THE BASIS OF PREVIOUS INFORMATION AND THE NEW CONVERSATION BETWEEN ME AND SIFRA, BUILD A SUMMARY ABOUT ALL THE INFORMATION ABOUT ME THAT SIFRA CAN KNOW FROM OUR CONVERATION SUCH THAT READING THAT SUMMARY, ANY ONE WOULD BE ABLE TO KNOW ABOUT ME AS MUCH SIFRA KNOWS AND RETURN THE SAME."
+        response = model.generate_content(prompt)
+        try:
+            candidate = response.candidates[0]
+            content = candidate.content
+            text_content = content.parts[0].text
+            return text_content
+        except Exception as e:
+            print(e)
+            return generateErrorResponse(e)
+
+
+def generateErrorResponse(messages: str):
+    prompt = f"{messages}\n\nCONSIDER THE ABOVE MESSAGES, THIS CONVERSATION IS NOT GOOD AND SAFE, RETURN A GOOD REASON FOR THE SAME. YOUR REPLY SHOULD BE IN FIRST PERSON PERSPECTIVE AS IF THESE CONVERSATION WERE WITH YOU, ALSO THE REPLY SHOULD BE SHORT AND CRISP\n\n\n\nNOTE: dont reveal that you are an ai, return a response like a human being about how this is inappropraite"
     response = model.generate_content(prompt)
     try:
         candidate = response.candidates[0]
--- routers/chat.py
+++ routers/chat.py
@@ -12,18 +12,15 @@ async def chat(message: message.Message, username: str = Depends(pass_jwt.get_cu
     if user:
         conversation = mongo.db.conversation.find_one({"username": username})
         if conversation:
-            conversation["conversation"].append(f"ME: {message.data}")
-            mongo.db.conversation.update_one({"username": username}, {"$set": {"conversation": conversation["conversation"]}})
-            response = model.generateResponse(conversation["conversation"])
-            conversation["conversation"].append(f"SIFRA: {response}")
-            mongo.db.conversation.update_one({"username": username}, {"$set": {"conversation": conversation["conversation"]}})
+            prevContext = conversation["conversation"]
+            response = model.resumeConversation(prevContext, message.data)
+            context = model.makeContext(message.data, response, prevContext)
+            mongo.db.conversation.update_one({"username": username}, {"$set": {"conversation": context}})
             return JSONResponse(content={"response": response}, status_code=200)
         else:
-            data = [message.data]
-            mongo.db.conversation.update_one({"username": username}, {"$set": {"conversation": data}})
-            response = model.generateFirstResponse(data)
-            data.append(response)
-            mongo.db.conversation.insert_one({"username": username, "conversation": data})
+            response = model.startConversation(message.data)
+            context = model.makeContext(message.data, response)
+            mongo.db.conversation.insert_one({"username": username, "conversation": context})
             return JSONResponse(content={"response": response}, status_code=200)
     else:
         return JSONResponse(content={"error": "Unauthenticated"}, status_code=404)
