27cd69d103ded45280e9b70079a26f9fe73ad233
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-07-11 14:36:41 +0530

    specified origins for api calls

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(chat.router)
 
-origins = ["*"]
+origins = [
+    "https://sifra-prerelease.netlify.app",  
+    "https://sifra.swoyam.in", 
+    "http://localhost",                     
+    "http://localhost:5173",                 
+    "http://127.0.0.1",                      
+    "http://127.0.0.1:5173"                  
+]
 
 app.add_middleware(
     CORSMiddleware,
