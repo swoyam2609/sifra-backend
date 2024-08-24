3e7fbf1357f1d3f325014516d173c36ae0eef7ca
Swoyam Siddharth Nayak <me@swoyam.in>
2024-08-24 19:34:05 +0530

    added tfug

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ origins = [
     "http://localhost",                     
     "http://localhost:5173",                 
     "http://127.0.0.1",                      
-    "http://127.0.0.1:5173"                  
+    "http://127.0.0.1:5173",
+    "https://sifra.tfugbbsr.in"
 ]
 
 app.add_middleware(
