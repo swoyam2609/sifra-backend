0e690d3fec005308d3c8b4cd098b632cd380f102
Swoyam Siddharth Nayak <me@swoyam.in>
2024-09-28 17:33:20 +0530

    added all origin access

--- main.py
+++ main.py
@@ -9,15 +9,7 @@ app.include_router(user.router)
 app.include_router(chat.router)
 app.include_router(sifraextended.router)
 
-origins = [
-    "https://sifra-prerelease.netlify.app",  
-    "https://sifra.swoyam.in", 
-    "http://localhost",                     
-    "http://localhost:5173",                 
-    "http://127.0.0.1",                      
-    "http://127.0.0.1:5173",
-    "https://sifra.tfugbbsr.in"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
